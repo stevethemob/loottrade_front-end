bc63c11baf1739d8e87c295416e216de4b1de8d3
Laurensdegroot1 <150124584+Laurensdegroot1@users.noreply.github.com>
2025-12-12 10:59:15 +0100

    added linting to the front-end, run it by typing in the terminal: npm run lint

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { registerUser } from "../api/user-api";
-import "../css/Login.css";
+import "../css/login.css";
 
 
 export default function Signup() {
